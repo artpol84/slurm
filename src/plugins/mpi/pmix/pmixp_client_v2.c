a9ba870a203328ad0e5c9bd461298865cc890739
Artem Y. Polyakov <artemp@nvidia.com>
2025-02-10 15:22:18 -0500

    Do not kill application (fix1)

--- src/plugins/mpi/pmix/pmixp_client_v2.c
+++ src/plugins/mpi/pmix/pmixp_client_v2.c
@@ -208,7 +208,7 @@ static void _errhandler(size_t evhdlr_registration_id,
 {
 	/* TODO: do something more sophisticated here */
 	/* FIXME: use proper specificator for nranges */
-	PMIXP_ERROR("Error handler invoked: status = %d, source = [%s:%d]",
+	PMIXP_ERROR("NEW!!!!!! Error handler invoked: status = %d, source = [%s:%d]",
 		    (int) status, source->nspace, source->rank);
 //	slurm_kill_job_step(pmixp_info_jobid(), pmixp_info_stepid(), SIGKILL,
 //			    0);
